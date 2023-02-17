29241078995a92749e598a245b09597585bc47ff
Auh3b <ngaiyayec@yahoo.com>
2023-02-17 13:24:35 +0200

    added media components

---
+++ src/components/common/media/MediaContainer.tsx
@@ -0,0 +1,84 @@
+import { Card, Paper,makeStyles, Tab, Tabs, Typography } from "@material-ui/core";
+import { useEffect, useState } from "react";
+import  {CustomTheme } from "theme"
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+  className: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, className,...other } = props;
+
+  return (
+    <div
+      className={className}
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <div>
+          <Typography>{children}</Typography>
+        </div>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+     margin: 10,
+  },
+  content:{
+    padding: 10,
+  }
+}));
+
+export default function MediaContainer({...args}) {
+  const classes = useStyles()
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event:any, newValue:number) => {
+    setValue(newValue);
+  };
+
+  useEffect(() => {
+    console.log(value)
+  
+    return () => {
+      console.log("done")
+    }
+  }, [value])
+  
+  
+  return (
+    <div className={classes.card}>
+      <Tabs value={value} onChange={handleChange} aria-label='tabs example' {...args}>
+        <Tab label='Facebook' {...a11yProps(0)}/>
+        <Tab label='Twitter' {...a11yProps(1)}/>
+        <Tab label='WhatsApp' {...a11yProps(2)}/>
+      </Tabs>
+      <TabPanel className={classes.content} value={value} index={0}>
+        FaceBook Cotent
+      </TabPanel>
+      <TabPanel className={classes.content} value={value} index={1}>
+        Twitter Content
+      </TabPanel>
+      <TabPanel className={classes.content} value={value} index={2}>
+        WhatsApp Content
+      </TabPanel>
+    </div>
+  )
+}
--- src/components/views/Media.tsx
+++ src/components/views/Media.tsx
@@ -1,5 +1,38 @@
 import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from '@material-ui/core';
+import MediaContainer from 'components/common/media/MediaContainer';
+import { FormulaWidget, PieWidget } from '@carto/react-widgets';
+import hotspotSource from "data/sources/hotspotSource"
+import { AggregationTypes } from '@carto/react-core';
+import { PieWidgetUI } from '@carto/react-ui';
+
+
+const colors = [
+  '#1877f2',
+  '#075e54',
+  '#1da1f2',
+  '#9146ff'
+]
+
+const data = [
+  {
+    name: 'Facebook',
+    value: Math.floor(5050*0.45)
+  },
+  {
+    name: 'WhatsApp',
+    value: Math.floor(5050*0.25)
+  },
+  {
+    name: 'Twitter',
+    value: Math.floor(5050*0.20)
+  },
+  {
+    name: 'Others',
+    value: Math.floor(5050*0.10)
+  }
+]
+
 
 const useStyles = makeStyles(() => ({
   media: {},
@@ -12,7 +45,26 @@ export default function Media() {
 
   return (
     <Grid container direction='column' className={classes.media}>
-      <Grid item></Grid>
+      <Grid item>
+        <FormulaWidget 
+          id='participantCount'
+          title='Participación por red social'
+          dataSource={hotspotSource.id}
+          column='carto_10_e'
+          operation={AggregationTypes.SUM}
+        />
+      </Grid>
+      <Grid item>
+        <PieWidgetUI 
+          id='participationGroups'
+          title='Participación por red social'
+          data={data}
+          color={colors}
+        />
+      </Grid>
+      <Grid item>
+        <MediaContainer />
+      </Grid>
     </Grid>
   );
 }
