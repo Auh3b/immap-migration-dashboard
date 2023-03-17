ed9f6e751ab522dd127d36685e0736ec9853c1e0
Auh3b <ngaiyayec@yahoo.com>
2023-03-17 12:58:31 +0200

    removed broken map

--- src/components/views/Media.tsx
+++ src/components/views/Media.tsx
@@ -8,6 +8,7 @@ import DeckGLComponent from 'components/common/map/DeckGLComponent';
 import TopLoading from 'components/common/TopLoading';
 import { useDispatch } from 'react-redux';
 import { setViewState } from '@carto/react-redux';
+import MainView from './main/MainView';
 
 const useStyles = makeStyles((theme) => ({
   mapWrapper: {
@@ -30,74 +31,20 @@ const useStyles = makeStyles((theme) => ({
       },
     },
   },
-  zoomControl: {
-    position: 'absolute',
-    bottom: theme.spacing(4),
-    left: theme.spacing(4),
-    zIndex: 1,
-
-    [theme.breakpoints.down('xs')]: {
-      display: 'none',
-    },
-  },
-  legend: {
-    position: 'absolute',
-    bottom: theme.spacing(4),
-    right: theme.spacing(4),
-
-    [theme.breakpoints.down('sm')]: {
-      bottom: theme.spacing(10),
-      right: theme.spacing(2),
-    },
-  },
-  cartoLogoMap: {
-    position: 'absolute',
-    bottom: theme.spacing(4),
-    left: '50%',
-    transform: 'translateX(-50%)',
-  },
-  gmaps: {
-    '& $zoomControl': {
-      left: theme.spacing(4),
-      bottom: theme.spacing(5),
-    },
-  },
 }));
-
-const cartoMapId = '9ddf6a22-3bdf-4b41-9d6a-80ea80895d32';
 
 export default function Media() {
   const classes = useStyles();
-  const [layers, setLayers] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    async function fetchMapLayers() {
-      setIsLoading(true);
-      const { initialViewState, layers } = await fetchMap({
-        cartoMapId,
-      });
-      // dispatch(setBasemap(mapStyle.styleType))
-      dispatch(setViewState(initialViewState));
-      setLayers(layers);
-      setIsLoading(false);
-    }
-
-    fetchMapLayers();
-
-    return () => {
-      setLayers([]);
-    };
-  }, []);
-
-  // [hygen] Add useEffect
 
   return (
     <Grid item className={classes.mapWrapper}>
-      {isLoading ? <TopLoading /> : ''}
-      <DeckGLComponent layers={layers} />
+      {/* {isLoading ? <TopLoading /> : ''}
+      <DeckGLComponent layers={layers} /> */}
+      <MainView>
+        {{
+
+        }}
+      </MainView>
     </Grid>
   );
 }
