d96faa14ff5311c83bd42b7ad8789bccd9542963
Auh3b <ngaiyayec@yahoo.com>
2023-03-30 00:50:26 +0200

    feat: hex to rgb conversion function

---
+++ src/utils/d3Hex2RGB.ts
@@ -0,0 +1,6 @@
+import { color } from "d3";
+import { UNICEF_COLORS } from "theme";
+
+export default function d3Hex2RGB(index: number){
+  return [...Object.values(color(UNICEF_COLORS[index])).slice(0, -1)]
+}
