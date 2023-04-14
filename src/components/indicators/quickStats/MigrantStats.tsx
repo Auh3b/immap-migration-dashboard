f4ddedfc546b083839f84bbc715f850bc62444f7
Auh3b <ngaiyayec@yahoo.com>
2023-04-14 12:27:15 +0200

    fix: applied fix to broken fetch

--- src/components/indicators/quickStats/MigrantStats.tsx
+++ src/components/indicators/quickStats/MigrantStats.tsx
@@ -9,6 +9,10 @@ import { format } from 'd3';
 import { executeSQL } from '@carto/react-api';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store/store';
+import mainSource from 'data/sources/mainSource'
+import premiseSource from 'data/sources/premiseSource'
+// @ts-ignore
+import { FORMATS, fetchLayerData } from '@deck.gl/carto';
 
 const useStyles = makeStyles((theme) => ({
   statsContainer: {
@@ -45,28 +49,22 @@ export default function MigrantStats({ isOpen }: { isOpen: boolean }) {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
-  const credentials = useSelector(
-    (state: RootState) => state.carto.credentials,
-  );
+
   const fetchPremise = async () => {
-    const result = await executeSQL({
-      credentials,
-      connection: 'carto_dw',
-      query: 'SELECT * FROM shared.Premise_22032023',
-      opts: {
-        format: 'json',
-      },
+    const {data: result} = await fetchLayerData({
+      source: premiseSource.data,
+      type: premiseSource.type,
+      connection: premiseSource.connection,
+      format: FORMATS.JSON
     });
     return result;
   };
   const fetchAurora = async () => {
-    const result = await executeSQL({
-      credentials,
-      connection: 'carto_dw',
-      query: 'SELECT * FROM shared.LACRO_Marzo_2023',
-      opts: {
-        format: 'json',
-      },
+    const {data: result} = await fetchLayerData({
+      source: mainSource.data,
+      type: mainSource.type,
+      connection: mainSource.connection,
+      format: FORMATS.JSON
     });
     return result;
   };
