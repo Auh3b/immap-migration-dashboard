20f2e3f70cd3fdc54c639349d5e370b0f33df5a2
Auh3b <ngaiyayec@yahoo.com>
2023-04-24 15:32:42 +0200

    added hooks for intro page indicators

---
+++ src/components/indicators/introduction/hooks/useCategoryChange.ts
@@ -0,0 +1,39 @@
+import { useCallback } from "react";
+import { useDispatch } from "react-redux";
+import { addIntroFilter, removeIntroFilter } from "store/introSlice";
+
+export default function useIntroCategoryChange({
+  source,
+  column,
+  owner
+}:{
+  source: string
+  column:string
+  owner:string
+}) {
+  const dispatch = useDispatch()
+     const handleSelectedCategoriesChange = useCallback(
+    (categories) => {
+      if (categories && categories.length) {
+        dispatch(
+          addIntroFilter({
+            source,
+            column,
+            values: categories,
+            owner,
+          }),
+        );
+      } else {
+        dispatch(
+          removeIntroFilter({
+            source,
+            column,
+            owner,
+          }),
+        );
+      }
+    },
+    [column, owner, dispatch],
+  );
+  return handleSelectedCategoriesChange
+}
