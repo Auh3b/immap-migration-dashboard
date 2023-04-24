f05c96512af2dbbda4fa24f237b0ba0cb8ffaa7a
Auh3b <ngaiyayec@yahoo.com>
2023-04-24 16:47:14 +0200

    added styling

--- src/components/common/ComponentFallback.tsx
+++ src/components/common/ComponentFallback.tsx
@@ -5,15 +5,18 @@ const useFallbackStyles = makeStyles((theme) => ({
     width: '100%',
     height: '100%',
   },
-  progress:{
-    
-  }
+  progress: {},
 }));
 
 export default function ComponentFallback() {
   const classes = useFallbackStyles();
   return (
-    <Grid container justifyContent='center' alignItems='center' className={classes.root}>
+    <Grid
+      container
+      justifyContent='center'
+      alignItems='center'
+      className={classes.root}
+    >
       <CircularProgress color='inherit' />
     </Grid>
   );
