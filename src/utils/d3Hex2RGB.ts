8da4d03be1cb53f2dbd7f71b0b295dbf3a54b779
Auh3b <ngaiyayec@yahoo.com>
2023-04-26 00:23:40 +0200

    extended color option for conversion

--- src/utils/d3Hex2RGB.ts
+++ src/utils/d3Hex2RGB.ts
@@ -1,6 +1,14 @@
 import { color } from 'd3';
 import { UNICEF_COLORS } from 'theme';
 
-export default function d3Hex2RGB(index: number) {
-  return [...Object.values(color(UNICEF_COLORS[index])).slice(0, -1)];
+export default function d3Hex2RGB(value: number | string) {
+  let colorValue: string | number;
+
+  if (typeof value === 'string') {
+    colorValue = value;
+  } else {
+    colorValue = UNICEF_COLORS[value];
+  }
+
+  return [...Object.values(color(colorValue)).slice(0, -1)];
 }
