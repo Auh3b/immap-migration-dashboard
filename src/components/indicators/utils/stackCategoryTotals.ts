9e22341c99efb426dbf324c92bd58c4a953d5806
Auh3b <ngaiyayec@yahoo.com>
2023-04-25 19:02:06 +0200

    separated rollup function

---
+++ src/components/indicators/utils/stackCategoryTotals.ts
@@ -0,0 +1,38 @@
+import { descending, rollup } from "d3";
+
+export default function stackCategoryTotals({
+input,
+labels,
+}:{
+  input: {key: string, value: string}[]
+  labels: any
+}) {
+   const _groups = Array.from(
+    rollup(
+      input,
+      (v) => v.length,
+      (d) => d.key,
+      (d) => d.value,
+    ),
+  );
+
+  let groups: any[] = [];
+
+  for (let [name, valueMap] of _groups) {
+    let newValue: any[] = [['name', name]];
+    let total = 0;
+    //@ts-ignore
+    Array.from(labels).forEach(([key, value]) => {
+      newValue = [...newValue, [value, valueMap.get(value) ?? 0]];
+      total += valueMap.get(value) ?? 0;
+    });
+    newValue = newValue.map(([key, value], index) => {
+      if (index === 0) {
+        return [key, value];
+      }
+      return [key, value / total];
+    });
+    groups = [...groups, Object.fromEntries(newValue)];
+  }
+  return groups.sort((a, b) => descending(a.name, b.name))
+}
--- src/components/indicators/utils/stackedGroupCategoriesAlt2.ts
+++ src/components/indicators/utils/stackedGroupCategoriesAlt2.ts
@@ -1,6 +1,7 @@
 import { AggregationTypes, groupValuesByColumn } from '@carto/react-core';
 import MethodFunc from './methodType';
 import { descending, rollup } from 'd3';
+import stackCategoryTotals from './stackCategoryTotals';
 
 const stackedGroupCategoriesAlt2: MethodFunc = (
   input: any[],
@@ -10,60 +11,19 @@ const stackedGroupCategoriesAlt2: MethodFunc = (
   //@ts-ignore
   const { aidTypes, labels, valueColumn } = methodParams;
   //@ts-ignore
-  const values: any[] = input.map((d)=> ({key: aidTypes.get(d[column]) , value: labels.get(d[valueColumn]) }))
-  // for (let f of input) {
-  //   //@ts-ignore
-  //   const value = f[valueColumn].split(',');
-  //   const key = f[column].split(',');
-  //   for (let i = 0; i < key.length; i++) {
-  //     if (value[i]) {
-  //       values.push({
-  //         //@ts-ignore
-  //         key: aidTypes.get(+key[i]),
-  //         //@ts-ignore
-  //         value: +value[i] === 999999 ? labels.get(0) : labels.get(+value[i]),
-  //       });
-  //     } else {
-  //       values.push({
-  //         //@ts-ignore
-  //         key: aidTypes.get(+key[i]),
-  //         //@ts-ignore
-  //         value: labels.get(0),
-  //       });
-  //     }
-  //   }
-  // }
-
-  const _groups = Array.from(
-    rollup(
-      values,
-      (v) => v.length,
-      (d) => d.key,
-      (d) => d.value,
-    ),
-  );
-
-  let groups: any[] = [];
-
-  for (let [name, valueMap] of _groups) {
-    let newValue: any[] = [['name', name]];
-    let total = 0;
+  const values: any[] = input.map((d) => ({
     //@ts-ignore
-    Array.from(labels).forEach(([key, value]) => {
-      newValue = [...newValue, [value, valueMap.get(value) ?? 0]];
-      total += valueMap.get(value) ?? 0;
-    });
-    newValue = newValue.map(([key, value], index) => {
-      if (index === 0) {
-        return [key, value];
-      }
-      return [key, value / total];
-    });
-    groups = [...groups, Object.fromEntries(newValue)];
-  }
+    key: aidTypes.get(d[column]),
+    //@ts-ignore
+    value: labels.get(d[valueColumn]),
+  }));
 
-  return groups.sort((a, b) => descending(a.name, b.name));
+  const output = stackCategoryTotals({
+    input: values,
+    labels
+  })
+
+  return output;
 };
-
 
 export default stackedGroupCategoriesAlt2;
