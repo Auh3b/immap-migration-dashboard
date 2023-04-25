95c95914d58f62743bc60126165421d650ac02c5
Auh3b <ngaiyayec@yahoo.com>
2023-04-25 19:03:24 +0200

    change expandable default prop value

--- src/components/common/customWidgets/CustomWidgetWrapper.tsx
+++ src/components/common/customWidgets/CustomWidgetWrapper.tsx
@@ -93,7 +93,7 @@ const useStyles = makeStyles((theme) => ({
 export default function CustomWidgetWrapper({
   title,
   isLoading,
-  expandable,
+  expandable=true,
   expanded,
   onError,
   children,
