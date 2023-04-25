2181d91e3d13de59039b11dc3350817044f3df64
Auh3b <ngaiyayec@yahoo.com>
2023-04-25 15:30:29 +0200

    added data manipulation functions

--- src/components/indicators/introduction/TopServices.tsx
+++ src/components/indicators/introduction/TopServices.tsx
@@ -3,14 +3,66 @@ import TitleWrapper from './utils/TitleWrapper';
 import { IndicatorProps } from './utils/introductionTypes';
 import MethodFunc from '../utils/methodType';
 import { AggregationTypes, groupValuesByColumn } from '@carto/react-core';
+import { UNICEF_COLORS } from 'theme';
+import { SERVICES_KEY, SERVICE_STAT_COLUMNS } from '../premise/utils/services';
 
 const title = 'Top 3 de servicios sobrepasados en su capacidad';
 const subtitle = '';
-const column = '';
+const column = 'serv_tipo1';
 const id = 'topServices';
 const source = 'premiseData';
 
-const method: MethodFunc = (input, column, params) => {
+const otherColumns = {
+  country: 'ubicacion_',
+  region: 'lugar_enc',
+  organisation: 'org_pert',
+  persons: 'nna_atend',
+};
+
+const SERVICE_STAT_COLUMNS_NAME = [
+  'Capacidad diaria',
+  'Personas atendidas ayer',
+  'Promedio diario',
+];
+
+const STAT_CATEGORY_COLORS = new Map([
+  ['Capacidad diaria', UNICEF_COLORS[5]],
+  ['Personas atendidas ayer', UNICEF_COLORS[4]],
+  ['Promedio diario', UNICEF_COLORS[0]],
+]);
+
+const firstMethod: MethodFunc = (input, column, params) => {
+  let output: any[] = [];
+  const { otherColumns } = params;
+
+  for (let serviceEntry of input) {
+    const services: number[] = serviceEntry[column]
+      .split(',')
+      .map((d: string) => +d);
+
+    services.forEach((service) => {
+      const serviceColumns = SERVICE_STAT_COLUMNS.get(service);
+      let newEntry: any = [
+        SERVICES_KEY.get(service),
+        serviceEntry[otherColumns.country],
+        serviceEntry[otherColumns.region],
+        serviceEntry[otherColumns.organisation],
+        serviceEntry[otherColumns.persons],
+        `${serviceEntry[otherColumns.organisation]} - ${SERVICES_KEY.get(
+          service,
+        )}`,
+      ];
+      for (let i = 0; i < SERVICE_STAT_COLUMNS_NAME.length; i++) {
+        newEntry = [...newEntry, serviceEntry[serviceColumns[i]] || 0];
+      }
+      output = [...output, newEntry];
+    });
+  }
+  console.log(output);
+  return output;
+};
+
+const secondMethod: MethodFunc = (input, column, params) => {
   let output: any[] = [];
   const { columns: valueColumns } = params;
 
