d6155e03eeddbac49dfbe98d32efd36b641f265e
Auh3b <ngaiyayec@yahoo.com>
2023-04-26 00:21:43 +0200

    extend key options

--- src/components/indicators/utils/stackedGroupCategoriesAlt2.ts
+++ src/components/indicators/utils/stackedGroupCategoriesAlt2.ts
@@ -8,20 +8,21 @@ const stackedGroupCategoriesAlt2: MethodFunc = (
   column: string,
   methodParams?: Record<string, unknown>,
 ) => {
+
   //@ts-ignore
   const { aidTypes, labels, valueColumn } = methodParams;
   //@ts-ignore
   const values: any[] = input.map((d) => ({
     //@ts-ignore
-    key: aidTypes.get(d[column]),
+    key: aidTypes.get(d[column]) ?? d[column],
     //@ts-ignore
     value: labels.get(d[valueColumn]),
   }));
 
   const output = stackCategoryTotals({
     input: values,
-    labels
-  })
+    labels,
+  });
 
   return output;
 };
