5d090c3464512107850e055b49d8a2fe99222759
Auh3b <ngaiyayec@yahoo.com>
2023-04-26 18:24:57 +0200

    added heat map layer

--- src/components/layers/ServiceConcentrationLayer.tsx
+++ src/components/layers/ServiceConcentrationLayer.tsx
@@ -1,16 +1,19 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 //@ts-ignore
 import { HeatmapLayer } from '@deck.gl/aggregation-layers';
 // @ts-ignore
 import { fetchLayerData } from '@deck.gl/carto';
-import { selectSourceById } from '@carto/react-redux';
+import { addLayer, removeLayer, updateLayer } from '@carto/react-redux';
 import { RootState } from 'store/store';
 import premiseSource from 'data/sources/premiseSource'
 import { useEffect, useState } from 'react';
+import { LEGEND_TYPES } from '@carto/react-ui';
+import d3Hex2RGB from 'utils/d3Hex2RGB';
 
 export const SERVICE_CONCENTRATION_LAYER_ID = 'serviceConcentrationLayer';
 
 export default function ServiceConcentrationLayer() {
+  const dispatch = useDispatch()
   const [data, setData] = useState(null)
   const { serviceConcentrationLayer } = useSelector((state: RootState) => state.carto.layers);
 
@@ -25,9 +28,10 @@ export default function ServiceConcentrationLayer() {
 
   useEffect(()=>{
     fetchData()
+    return ()=>{
+      dispatch(removeLayer(SERVICE_CONCENTRATION_LAYER_ID))
+    }
   }, [])
-
-  console.log(serviceConcentrationLayer)
 
   if (serviceConcentrationLayer && data) {
     return new HeatmapLayer({
@@ -38,9 +42,32 @@ export default function ServiceConcentrationLayer() {
       getPosition: (d:any) => d.geom.coordinates,
       getWeight: (d:any) => d.porc_sobre,
       intensity: 1,
+      visible: serviceConcentrationLayer.visible,
       threshold: 0.3,
-      onDataLoad:(data:any, context:any)=>{
-        console.log(data)
+      onDataLoad:()=>{
+        dispatch(updateLayer({
+          id: SERVICE_CONCENTRATION_LAYER_ID,
+          layerAttributes:{
+            title: 'Service Concentration',
+            legend: {
+                attr: 'hotspot',
+                type: LEGEND_TYPES.CONTINUOUS_RAMP,
+                labels: [
+                  {
+                    label: 'Bajo',
+                    value: 0
+                  },
+                  {
+                    label: 'Bajo',
+                    value: 1
+                  },
+                ],
+                colors: [
+                  d3Hex2RGB('#ffffb2'), d3Hex2RGB('#bd0026')
+                ],
+            },
+          }
+        }))
       }
     });
   }
