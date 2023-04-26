2bb75dc58c0994bd4b25a2cfe5c8e8d06c25a96c
Auh3b <ngaiyayec@yahoo.com>
2023-04-26 18:37:00 +0200

    changed legend props

--- src/components/layers/PremiseServicesLayer.tsx
+++ src/components/layers/PremiseServicesLayer.tsx
@@ -27,8 +27,6 @@ const layerConfig = {
     type: LEGEND_TYPES.CATEGORY,
     labels: DATA.map((data) => data.label),
     colors: DATA.map((data) => data.color),
-    colllapsible: true,
-    collapsed: true,
   },
 };
 
