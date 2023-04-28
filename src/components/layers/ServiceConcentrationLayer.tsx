8e33a4e43aacbd736a6d6fc2e14086a686c26923
Auh3b <ngaiyayec@yahoo.com>
2023-04-28 18:34:35 +0200

    used another useWidgetFetch to sync with widget filtering

--- src/components/layers/ServiceConcentrationLayer.tsx
+++ src/components/layers/ServiceConcentrationLayer.tsx
@@ -9,27 +9,34 @@ import premiseSource from 'data/sources/premiseSource';
 import { useEffect, useState } from 'react';
 import { LEGEND_TYPES } from '@carto/react-ui';
 import d3Hex2RGB from 'utils/d3Hex2RGB';
+import useWidgetFetch from 'components/common/customWidgets/hooks/useWidgetFetch';
+import { SURVEY_CONCENTRATIONS_LAYER_ID } from './SurveyConcentrationsLayer';
 
 export const SERVICE_CONCENTRATION_LAYER_ID = 'serviceConcentrationLayer';
 
 export default function ServiceConcentrationLayer() {
   const dispatch = useDispatch();
-  const [data, setData] = useState(null);
+  // const [data, setData] = useState(null);
   const { serviceConcentrationLayer } = useSelector(
     (state: RootState) => state.carto.layers,
   );
 
-  const fetchData = async () => {
-    const { data } = await fetchLayerData({
-      ...premiseSource,
-      source: premiseSource.data,
-      format: 'json',
-    });
-    setData(data);
-  };
+  // const fetchData = async () => {
+  //   const { data } = await fetchLayerData({
+  //     ...premiseSource,
+  //     source: premiseSource.data,
+  //     format: 'json',
+  //   });
+  //   setData(data);
+  // };
+
+  const {data} = useWidgetFetch({
+    id: SURVEY_CONCENTRATIONS_LAYER_ID,
+    dataSource: premiseSource.id
+  })
 
   useEffect(() => {
-    fetchData();
+    // fetchData();
      dispatch(
           updateLayer({
             id: SERVICE_CONCENTRATION_LAYER_ID,
@@ -58,12 +65,14 @@ export default function ServiceConcentrationLayer() {
     };
   }, []);
 
-  if (serviceConcentrationLayer && data) {
+  console.log(data)
+
+  if (serviceConcentrationLayer && data.length> 0) {
     return new HeatmapLayer({
       id: SERVICE_CONCENTRATION_LAYER_ID,
       data: new Promise((resolve) => resolve(data)),
       opacity: 0.8,
-      getPosition: (d: any) => d.geom.coordinates,
+      getPosition: (d: any) => [d.long, d.lat],
       getWeight: (d: any) => d.porc_sobre,
       intensity: 1,
       visible: serviceConcentrationLayer.visible,
