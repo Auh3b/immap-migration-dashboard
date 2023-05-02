fc88a839609edeaffbce7604e3b3383426ab6181
Auh3b <ngaiyayec@yahoo.com>
2023-05-02 22:41:23 +0200

    added pop variables

--- src/components/layers/utils/premisePopup.ts
+++ src/components/layers/utils/premisePopup.ts
@@ -1,13 +1,19 @@
+export default function premisePopup({ data }: any) {
+  console.log(data);
+  const {
+    org_pert1: implementor,
+    org_pert2: principal,
+    pqr_migran: complaints,
+    serv_dif_n: childrenServicesAvailability,
+    serv_dif_m: womenServicesAvailability,
+  } = data;
 
-export default function premisePopup({data}:any) {
-  
-  const {org_pert1: implementor, org_pert2: principal} = data
-  
-  return (
-    `<div style='display: flex; flex-direction: column; width: 150px;'>
+  return `<div style='display: flex; flex-direction: column; width: 150px;'>
       <p>Organización</p>
       <p>Socio Implementador: ${implementor}</p>
-      <p>Socio Principal: ${principal}</p> 
-    </div>`
-  )
+      <p>Socio Principal: ${principal}</p>
+      <p>Servicios diferenciados para ninos: ${childrenServicesAvailability}</p>
+      <p>Servicios diferenciados para mujeres: ${womenServicesAvailability}</p>
+      <p>Recibe PQR migrante: ${complaints}</p>
+    </div>`;
 }
