0c958ae559a180dbd503477ca92f9ff687b213ec
Auh3b <ngaiyayec@yahoo.com>
2023-05-02 22:41:51 +0200

    re-ordered indicators

--- src/components/views/premiseViews/PremiseLeftView.tsx
+++ src/components/views/premiseViews/PremiseLeftView.tsx
@@ -1,42 +1,22 @@
 import { MainColumnView } from 'components/common/MainColumnView';
-// import DisabilityServices from 'components/indicators/premise/DisabilityServices';
-// import InfoLanguages from 'components/indicators/premise/InfoLanguages';
-// import LocationFeatures from 'components/indicators/premise/LocationFeatures';
-import OrgSurveyed from 'components/indicators/premise/OrgSurveyed';
-// import PremiseGenderComposition from 'components/indicators/premise/PremiseGenderComposition';
-// import ServiceLocation from 'components/indicators/premise/ServiceLocation';
-// import ServiceTime from 'components/indicators/premise/ServiceTime';
-// import WasteManagement from 'components/indicators/premise/WasteManagement';
-import ComplaintReporting from 'components/indicators/premise/ComplaintReporting';
-// import OtherLanguges from 'components/indicators/premise/OtherLanguges';
 import ViewSourceType from '../utils/viewSourceType';
-// import SocialServices from 'components/indicators/premise/SocialServices';
+import OrgSurveyed from 'components/indicators/premise/OrgSurveyed';
+import OrganisationPrincipals from 'components/indicators/premise/OrganisationPrincipals';
 import ServicesProvided from 'components/indicators/premise/ServicesProvided';
-// import OtherServiceTypes from 'components/indicators/premise/OtherServiceTypes';
+import ComplaintReporting from 'components/indicators/premise/ComplaintReporting';
 import WomenDiffServices from 'components/indicators/premise/WomenDiffServices';
 import WomenDiffServicesAvailability from 'components/indicators/premise/WomenDiffServicesAvailability';
-import OrganisationPrincipals from 'components/indicators/premise/OrganisationPrincipals';
 
 export default function PremiseLeftView({ dataSources }: ViewSourceType) {
   const { premiseSource } = dataSources;
   return (
     <MainColumnView>
-      {/* <PremiseGenderComposition dataSource={premiseSource} /> */}
-      {/* <SocialServices dataSource={premiseSource} /> */}
       <OrgSurveyed dataSource={premiseSource} />
       <OrganisationPrincipals dataSource={premiseSource} />
       <ServicesProvided dataSource={premiseSource} />
-      {/* <OtherServiceTypes dataSource={premiseSource} /> */}
-      {/* <ServiceTime dataSource={premiseSource} /> */}
-      <WomenDiffServicesAvailability dataSource={premiseSource} />
+      {/* <WomenDiffServicesAvailability dataSource={premiseSource} />
       <WomenDiffServices dataSource={premiseSource} />
-      {/* <ServiceLocation dataSource={premiseSource} /> */}
-      {/* <LocationFeatures dataSource={premiseSource} /> */}
-      {/* <DisabilityServices dataSource={premiseSource} /> */}
-      {/* <WasteManagement dataSource={premiseSource} /> */}
-      {/* <InfoLanguages dataSource={premiseSource} /> */}
-      {/* <OtherLanguges dataSource={premiseSource} /> */}
-      <ComplaintReporting dataSource={premiseSource} />
+      <ComplaintReporting dataSource={premiseSource} /> */}
     </MainColumnView>
   );
 }
