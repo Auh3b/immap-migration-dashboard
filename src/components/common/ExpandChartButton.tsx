46bac4ba68faf0d9bbedbb80f3f53a66654698b9
Auh3b <ngaiyayec@yahoo.com>
2023-05-10 11:14:07 +0200

    disable button when modal is open

--- src/components/common/ExpandChartButton.tsx
+++ src/components/common/ExpandChartButton.tsx
@@ -1,14 +1,17 @@
 import { IconButton } from '@material-ui/core';
 import ZoomOutMapIcon from '@material-ui/icons/ZoomOutMap';
-import { useDispatch } from 'react-redux';
-import { setChartModule } from 'store/appSlice';
-export default function ExpandChartButton() {
+import { useDispatch, useSelector } from 'react-redux';
+import { setChartModal, setModalUrl } from 'store/appSlice';
+export default function ExpandChartButton({ chartUrl }: { chartUrl: string }) {
   const dispatch = useDispatch();
   const handleExpandChart = () => {
-    dispatch(setChartModule(true));
+    dispatch(setChartModal(true));
+    dispatch(setModalUrl(chartUrl));
   };
+  //@ts-ignore
+  const showChartModal = useSelector(state => state.app.showChartModal) ?? false
   return (
-    <IconButton onClick={handleExpandChart}>
+    <IconButton disabled={showChartModal} onClick={handleExpandChart}>
       <ZoomOutMapIcon />
     </IconButton>
   );
