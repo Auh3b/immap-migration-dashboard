1413d1ba9b4a7f9384bda42e6101735937ed1376
Auh3b <ngaiyayec@yahoo.com>
2023-05-11 21:02:59 +0200

    added children aid type key

--- src/components/indicators/services/utils/serviceIndicatorTypes.ts
+++ src/components/indicators/services/utils/serviceIndicatorTypes.ts
@@ -14,6 +14,18 @@ export const aidTypes = new Map([
   [11, 'Otro'],
 ]);
 
+export const serviceTypeChildren = new Map([
+  [1, 'Alimentación o kit de alimentación'],
+  [2, 'Alojamiento temporal'],
+  [3, 'Duchas o baños'],
+  [4, 'Agua'],
+  [5, 'Kit de aseo o elementos de higiene'],
+  [6, 'Ayuda psicológica'],
+  [7, 'Espacios educativos o de cuidado'],
+  [8, 'Salud, primeros auxilios o atención médica'],
+  [9, 'Otro'],
+])
+
 export const serviceLabels1 = new Map([
   [3, 'Difícil'],
   [2, 'Regular'],
