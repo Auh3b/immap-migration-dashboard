7b566764f55e612dff2769e90ada04c9d00b4be5
Auh3b <ngaiyayec@yahoo.com>
2023-05-11 20:03:44 +0200

    added condition to remove button when activated

--- src/components/common/ExpandChartButton.tsx
+++ src/components/common/ExpandChartButton.tsx
@@ -1,7 +1,14 @@
-import { IconButton } from '@material-ui/core';
+import { IconButton, makeStyles } from '@material-ui/core';
 import ZoomOutMapIcon from '@material-ui/icons/ZoomOutMap';
 import { useDispatch, useSelector } from 'react-redux';
 import { setChartModal, setModalUrl } from 'store/appSlice';
+
+const useStyles = makeStyles((theme)=>({
+  root:{
+    display: ({show}:any)=> show ? 'none' : 'block',
+  }
+}))
+
 export default function ExpandChartButton({ chartUrl }: { chartUrl: string }) {
   const dispatch = useDispatch();
   const handleExpandChart = () => {
@@ -10,8 +17,9 @@ export default function ExpandChartButton({ chartUrl }: { chartUrl: string }) {
   };
   //@ts-ignore
   const showChartModal = useSelector(state => state.app.showChartModal) ?? false
+  const classes = useStyles({show: showChartModal})
   return (
-    <IconButton disabled={showChartModal} onClick={handleExpandChart}>
+    <IconButton className={classes.root} disabled={showChartModal} onClick={handleExpandChart}>
       <ZoomOutMapIcon />
     </IconButton>
   );
