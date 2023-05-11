9586f6554fba690365a8cc7c8cb44a69618752c9
Auh3b <ngaiyayec@yahoo.com>
2023-05-11 20:06:09 +0200

    Changed title

--- src/components/indicators/services/ServiceTypeChildren.tsx
+++ src/components/indicators/services/ServiceTypeChildren.tsx
@@ -3,14 +3,13 @@ import { Grid } from '@material-ui/core';
 import { BasicWidgetType } from 'components/common/customWidgets/basicWidgetType';
 import CustomCategoryWidget from 'components/common/customWidgets/CustomCategoryWidget';
 import WidgetNote from 'components/common/customWidgets/WidgetNote';
-import concatenatedValues from '../utils/concatenatedValues';
 import useWidgetEffect from '../utils/useWidgetEffect';
 import { aidTypes } from './utils/serviceIndicatorTypes';
 import groupCategories from '../utils/groupCategories';
 
 const NOTE = 'Ayudas humanitarias recibidas según zona de tránsito';
 const id = 'serviceTypeAdult';
-const title = 'Ayudas humanitarias';
+const title = 'Ayudas humanitarias NNA';
 const column = 'm18_me_con';
 const filterType = _FilterTypes.IN;
 const method = groupCategories;
