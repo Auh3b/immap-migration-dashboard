13838b002e14bbeb2a45de15869fd5d7f121c97c
Auh3b <ngaiyayec@yahoo.com>
2023-05-16 19:35:09 +0200

    added sources name

--- src/components/views/mediaViews/utils/mediaUtils.ts
+++ src/components/views/mediaViews/utils/mediaUtils.ts
@@ -28,6 +28,19 @@ export const MEDIA_SOURCES = {
   REDDIT: 'reddit',
 };
 
+export const MEDIA_SOURCES_NAMES = new Map([
+  [MEDIA_SOURCES.ALL, 'News'],
+  [MEDIA_SOURCES.FACEBOOK, 'Facebook'],
+  [MEDIA_SOURCES.TWITTER, 'Twitter'],
+  [MEDIA_SOURCES.TIKTOK, 'Tiktok'],
+  [MEDIA_SOURCES.REDDIT, 'Reddit'],
+  [MEDIA_SOURCES.SOCIAL_BLOGS, 'Blogs'],
+  [MEDIA_SOURCES.SOCIAL_COMMENTS, 'Comments'],
+  [MEDIA_SOURCES.SOCIAL_REVIEWS, 'Reviews'],
+  [MEDIA_SOURCES.SOCIAL_MESSAGE_BOARDS, 'Message Boards'],
+  [MEDIA_SOURCES.FORUMS, 'Forums'],
+]);
+
 export const FA_MAP = new Map([
   [MEDIA_SOURCES.ALL, faNewspaper],
   [MEDIA_SOURCES.FACEBOOK, faFacebook],
