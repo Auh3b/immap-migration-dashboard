7e435ebf33d22618416310a5292ba360b4d3a5f3
Auh3b <ngaiyayec@yahoo.com>
2023-05-16 11:10:50 +0200

    source indicator template

---
+++ src/components/indicators/media/utils/SourceIndictor.tsx
@@ -0,0 +1,66 @@
+import { FormulaWidgetUI } from '@carto/react-ui';
+import { Grid, Paper, Typography, makeStyles } from '@material-ui/core';
+import { ReactNode } from 'react';
+import { UNICEF_COLORS } from 'theme';
+
+interface SourceIndictorProps {
+  title: string;
+  data: number;
+  icon?: ReactNode;
+  formatter?: Function;
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(2),
+  },
+  paper: {
+    padding: theme.spacing(2),
+  },
+  title: {
+    ...theme.typography.subtitle1,
+    color: theme.palette.secondary.light,
+    fontSize: theme.spacing(2.5),
+    textTransform: 'uppercase',
+  },
+  icon: {
+    height: '100%',
+    '& .svg-inline--fa': {
+      height: theme.spacing(3),
+    },
+  },
+  value: {
+    '&  span': {
+      color: `${UNICEF_COLORS[0]} !important`,
+    },
+  },
+}));
+
+export default function SourceIndictor({
+  title: _title,
+  data,
+  icon,
+  formatter,
+}: SourceIndictorProps) {
+  const classes = useStyles();
+  const title = _title === 'all' ? 'news' : _title;
+  return (
+    <Grid item xs={12} md={4} lg={2} className={classes.root}>
+      <Paper variant='outlined' className={classes.paper}>
+        <Grid spacing={2} wrap='nowrap' container alignItems='center'>
+          <Grid item className={classes.icon}>
+            {icon}
+          </Grid>
+          <Grid item>
+            <Typography className={classes.title}>
+              {title.replaceAll('_', ' ')}
+            </Typography>
+            <span className={classes.value}>
+              <FormulaWidgetUI data={data} formatter={formatter} />
+            </span>
+          </Grid>
+        </Grid>
+      </Paper>
+    </Grid>
+  );
+}
