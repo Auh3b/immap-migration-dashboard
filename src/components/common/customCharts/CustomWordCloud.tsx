29dd94781515a591f2b42bd1b8df9fbafd63197b
Auh3b <ngaiyayec@yahoo.com>
2023-05-17 10:22:56 +0200

    add to option properties

--- src/components/common/customCharts/CustomWordCloud.tsx
+++ src/components/common/customCharts/CustomWordCloud.tsx
@@ -1,7 +1,7 @@
-import React, { useCallback, useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import ReactEcharts from './ReactEcharts';
 import cloud from 'd3-cloud';
-import { makeStyles } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/core';
 import { extent, interpolateCividis, scaleSequential } from 'd3';
 
 const width = 450;
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme)=>({
     },
     '& text':{
       dominantBaseline: 'unset',
-      y: 'unset'
+      y: '0'
     }
   }
 }))
@@ -32,9 +32,7 @@ export default function CustomWordCloud({
   data: { name: string; value: number }[];
 }) {
   const classes = useStyles({height})
-
-
-
+  const theme = useTheme()
   const data = useMemo(() => {
     if (_data.length === 0) {
       return [];
@@ -64,7 +62,6 @@ export default function CustomWordCloud({
   }, [_data]);
 
   const getColor = useCallback((value)=>{
-    console.log(data)
     const colorScale = scaleSequential(interpolateCividis).domain(extent(data, d=> d[3]))
     return colorScale(value)
   }, [_data])
@@ -94,6 +91,7 @@ export default function CustomWordCloud({
               text,
               font,
               textAlign: 'middle',
+              textVerticalAlign: 'middle',
               fill: getColor(size),
             },
           };
@@ -105,7 +103,24 @@ export default function CustomWordCloud({
   );
   const option = useMemo(
     () => ({
-      tooltip: {},
+      tooltip:{
+      padding: [theme.spacing(0.5), theme.spacing(1)],
+      borderWidth: 0,
+      textStyle: {
+        ...theme.typography.caption,
+        fontSize: 12,
+        lineHeight: 16,
+        color: theme.palette.common.white,
+      },
+      //@ts-ignore
+      backgroundColor: theme.palette.other.tooltip,
+      formatter(params:any){
+        console.log(params)
+        return (
+         `<span>${params.value}</span>`
+        )
+      }
+    },
       series,
     }),
     [series],
