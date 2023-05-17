a428de1dbb8f2c273a57e13e010ee826053da05c
Auh3b <ngaiyayec@yahoo.com>
2023-05-17 10:23:57 +0200

    added spacing to content

--- src/components/views/mediaViews/utils/MediaIndicators.tsx
+++ src/components/views/mediaViews/utils/MediaIndicators.tsx
@@ -12,6 +12,7 @@ const useStyles = makeStyles((theme) => ({
   content: {
     width: '100%',
     height: '100%',
+    padding: theme.spacing(2)
   },
 }));
 export default function MediaIndicators({ isLoading, children }: any) {
