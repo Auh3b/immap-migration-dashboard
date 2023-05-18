48a8085f092e344c5090f9b806527c753d83c533
Auh3b <ngaiyayec@yahoo.com>
2023-05-18 11:23:34 +0200

    remove unused icons

--- src/components/views/mediaViews/utils/mediaUtils.ts
+++ src/components/views/mediaViews/utils/mediaUtils.ts
@@ -1,10 +1,6 @@
 import {
-  faComments,
   faHashtag,
-  faMessage,
   faNewspaper,
-  faSquareRss,
-  faStar,
   faUser,
 } from '@fortawesome/free-solid-svg-icons';
 import {
@@ -12,6 +8,7 @@ import {
   faReddit,
   faTiktok,
   faTwitter,
+  faYoutube,
 } from '@fortawesome/free-brands-svg-icons';
 
 export const MEDIA_SOURCES = {
@@ -20,10 +17,7 @@ export const MEDIA_SOURCES = {
   FACEBOOK: 'facebook',
   TWITTER: 'twitter',
   TIKTOK: 'tiktok',
-  SOCIAL_BLOGS: 'social_blogs',
-  SOCIAL_REVIEWS: 'social_reviews',
-  SOCIAL_COMMENTS: 'social_comments',
-  SOCIAL_MESSAGE_BOARDS: 'social_message_boards',
+  YOUTUBE: 'youtube',
   FORUMS: 'forums',
   REDDIT: 'reddit',
 };
@@ -34,10 +28,7 @@ export const MEDIA_SOURCES_NAMES = new Map([
   [MEDIA_SOURCES.TWITTER, 'Twitter'],
   [MEDIA_SOURCES.TIKTOK, 'Tiktok'],
   [MEDIA_SOURCES.REDDIT, 'Reddit'],
-  [MEDIA_SOURCES.SOCIAL_BLOGS, 'Blogs'],
-  [MEDIA_SOURCES.SOCIAL_COMMENTS, 'Comments'],
-  [MEDIA_SOURCES.SOCIAL_REVIEWS, 'Reviews'],
-  [MEDIA_SOURCES.SOCIAL_MESSAGE_BOARDS, 'Message Boards'],
+  [MEDIA_SOURCES.YOUTUBE, 'Youtube'],
   [MEDIA_SOURCES.FORUMS, 'Forums'],
 ]);
 
@@ -47,10 +38,7 @@ export const FA_MAP = new Map([
   [MEDIA_SOURCES.TWITTER, faTwitter],
   [MEDIA_SOURCES.TIKTOK, faTiktok],
   [MEDIA_SOURCES.REDDIT, faReddit],
-  [MEDIA_SOURCES.SOCIAL_BLOGS, faSquareRss],
-  [MEDIA_SOURCES.SOCIAL_COMMENTS, faComments],
-  [MEDIA_SOURCES.SOCIAL_REVIEWS, faStar],
-  [MEDIA_SOURCES.SOCIAL_MESSAGE_BOARDS, faMessage],
+  [MEDIA_SOURCES.YOUTUBE, faYoutube],
   [MEDIA_SOURCES.FORUMS, faUser],
   [MEDIA_SOURCES.MENCIONES_TOTALES, faHashtag],
 ]);
