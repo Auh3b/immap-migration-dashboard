b2de461f59ef377341b5cd5440e36207de4a661c
Auh3b <ngaiyayec@yahoo.com>
2023-05-21 22:30:35 +0200

    added indicator from inicio

--- src/components/indicators/introduction/IntroChildTravelParty.tsx
+++ src/components/indicators/introduction/IntroChildTravelParty.tsx
@@ -1,10 +1,10 @@
-import TitleWrapper from "components/common/TitleWrapper";
-import stackedBarCategories from "../utils/stackedBarCategories";
-import WidgetNote from "components/common/customWidgets/WidgetNote";
-import { useMemo } from "react";
-import { UNICEF_COLORS } from "theme";
-import { useTheme } from "@material-ui/core";
-import ReactEcharts from "components/common/customCharts/ReactEcharts";
+import TitleWrapper from 'components/common/TitleWrapper';
+import stackedBarCategories from '../utils/stackedBarCategories';
+import WidgetNote from 'components/common/customWidgets/WidgetNote';
+import { useMemo } from 'react';
+import { UNICEF_COLORS } from 'theme';
+import { useTheme } from '@material-ui/core';
+import ReactEcharts from 'components/common/customCharts/ReactEcharts';
 
 const title = 'NNA no acompañados y separados';
 const NOTE =
@@ -17,66 +17,81 @@ const methodParams = {
   legend: ['No acompañados', 'Separados'],
 };
 
-export default function IntroChildTravelParty({
-  data: _data
-}: any) {
-   const theme = useTheme()
+export default function IntroChildTravelParty({ data: _data }: any) {
+  const theme = useTheme();
 
-  const data = useMemo(()=> {
-    if(_data){
+  const data = useMemo(() => {
+    if (_data) {
       //@ts-ignore
-      return method(_data, column, methodParams)[0]
+      return method(_data, column, methodParams)[0];
     }
-  } 
-  , [_data])
+  }, [_data]);
 
-  const series = useMemo(()=>[
-    {
-      name: data?.legend[0],
-      type: 'bar',
-      data: data?.value[0],
-      itemStyle: {}
-    },
-    {
-      name: data?.legend[1],
-      type: 'bar',
-      data: data?.value[1],
-      itemStyle: {}
-    }
-  ], [data])
-
-  const option = useMemo(()=>({
-    color: [UNICEF_COLORS[0], UNICEF_COLORS[4]],
-    legend:{
-      icon: 'circle',
-    },
-    tooltip:{
-      trigger: 'axis',
-      padding: [theme.spacing(0.5), theme.spacing(1)],
-      borderWidth: 0,
-      textStyle: {
-        ...theme.typography.caption,
-        fontSize: 16,
-        lineHeight: 16,
-        color: theme.palette.common.white,
+  const series = useMemo(
+    () => [
+      {
+        name: data?.legend[0],
+        type: 'bar',
+        data: data?.value[0],
+        itemStyle: {},
       },
-      //@ts-ignore
-      backgroundColor: theme.palette.other.tooltip,
-    },
-    xAxis:{
-      type: 'category',
-      data: data?.name,
-    },
-    yAxis:{
-      type: 'value',
-    },
-    series,
-  }), [series])
+      {
+        name: data?.legend[1],
+        type: 'bar',
+        data: data?.value[1],
+        itemStyle: {},
+      },
+    ],
+    [data],
+  );
+
+  const option = useMemo(
+    () => ({
+      color: [UNICEF_COLORS[0], UNICEF_COLORS[4]],
+      grid: {
+        top: '5%',
+        left: '5%',
+        right: '5%',
+        containLabels: true,
+      },
+      legend: {
+        icon: 'circle',
+        bottom: '0%',
+        left: '5%',
+        orient: 'vertical',
+      },
+      tooltip: {
+        trigger: 'axis',
+        padding: [theme.spacing(0.5), theme.spacing(1)],
+        borderWidth: 0,
+        textStyle: {
+          ...theme.typography.caption,
+          fontSize: 16,
+          lineHeight: 16,
+          color: theme.palette.common.white,
+        },
+        //@ts-ignore
+        backgroundColor: theme.palette.other.tooltip,
+      },
+      xAxis: {
+        type: 'category',
+        data: data?.name,
+        axisTick: {
+          show: false,
+        },
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series,
+    }),
+    [series],
+  );
 
   return (
     <TitleWrapper title={title}>
-      <ReactEcharts option={option} opts={{renderer: 'svg'}} />
-      <WidgetNote note={NOTE}/>
+      <ReactEcharts option={option} opts={{ renderer: 'svg' }} />
+      <WidgetNote note={NOTE} />
     </TitleWrapper>
-  )
+  );
 }
