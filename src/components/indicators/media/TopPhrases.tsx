ee31ad1eb1b81b24ec2d79bff6e0eb3cfede8fdd
Auh3b <ngaiyayec@yahoo.com>
2023-05-22 03:38:57 +0200

    added filtering capabilities

--- src/components/indicators/media/TopPhrases.tsx
+++ src/components/indicators/media/TopPhrases.tsx
@@ -3,19 +3,22 @@ import TitleWrapper from 'components/common/TitleWrapper';
 import CustomWordCloud from 'components/common/customCharts/CustomWordCloud';
 import { METHOD_NAMES } from 'components/views/mediaViews/utils/methodName';
 import useMediaData from './hooks/useMediaData';
+import { useSelector } from 'react-redux';
 
-const id = 'topPhrases';
+const id = 'top_phrases';
 
 export default function TopPhrases() {
   const { data, isLoading } = useMediaData({
     id,
     methodName: METHOD_NAMES.MEDIA_TOP_PHRASES,
   });
+  //@ts-ignore
+  const filters = useSelector((state) => state.media.filters?.meltwater) || {}
 
   return (
     <Grid xs={12} lg={4} item>
       <TitleWrapper title='Palabras asociadas' isLoading={isLoading}>
-        <CustomWordCloud data={data} />
+        <CustomWordCloud data={data} filters={filters} id={id} />
       </TitleWrapper>
     </Grid>
   );
