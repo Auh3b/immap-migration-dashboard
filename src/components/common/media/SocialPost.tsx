7beaf4fc9e39af95d1c18fc97ff9b962e0d2aa5b
Auh3b <ngaiyayec@yahoo.com>
2023-05-23 15:38:25 +0200

    styling

--- src/components/common/media/SocialPost.tsx
+++ src/components/common/media/SocialPost.tsx
@@ -1,4 +1,4 @@
-import { Grid, makeStyles } from '@material-ui/core';
+import { Grid, Typography, makeStyles } from '@material-ui/core';
 import { MEDIA_SOURCES } from 'components/views/mediaViews/utils/mediaUtils';
 import YoutubePost from './YoutubePost';
 import TwitterPost from './TwitterPost';
@@ -6,6 +6,12 @@ import TiktokPost from './TiktokPost';
 
 const usePostStyles = makeStyles((theme) => ({
   root: {},
+  title: {
+    marginBottom: theme.spacing(2),
+    ...theme.typography.subtitle1,
+    fontSize: theme.spacing(2.2),
+    fontWeight: 'bold',
+  },
 }));
 
 export default function SocialPost({
@@ -16,22 +22,25 @@ export default function SocialPost({
   source: string;
 }) {
   const classes = usePostStyles();
-  const setPost = (source:string, url:string) =>{
+  const setPost = (source: string, url: string) => {
     switch (source) {
-      case MEDIA_SOURCES.YOUTUBE:{
-        return <YoutubePost url={url}/>
+      case MEDIA_SOURCES.YOUTUBE: {
+        return <YoutubePost url={url} />;
       }
-      case MEDIA_SOURCES.TWITTER:{
-        return <TwitterPost url={url} />
+      case MEDIA_SOURCES.TWITTER: {
+        return <TwitterPost url={url} />;
       }
-      case MEDIA_SOURCES.TIKTOK:{
-        return <TiktokPost url={url} />
+      case MEDIA_SOURCES.TIKTOK: {
+        return <TiktokPost url={url} />;
       }
     }
-  }
+  };
 
   return (
     <Grid item className={classes.root}>
+      <Typography variant='subtitle1' className={classes.title}>
+        {'TOP ' + source.toUpperCase() + ' POST'}
+      </Typography>
       {setPost(source, url)}
     </Grid>
   );
