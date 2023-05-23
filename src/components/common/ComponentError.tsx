1284be4f17dee02bdf17f6ca81f9bc597bc263ff
Auh3b <ngaiyayec@yahoo.com>
2023-05-23 13:54:13 +0200

    added error component

---
+++ src/components/common/ComponentError.tsx
@@ -0,0 +1,30 @@
+import { Grid, Typography, makeStyles } from '@material-ui/core';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
+
+const useErrorStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    Height: '100%',
+    minWidth: '150px',
+    minHeight: '200px',
+    padding: theme.spacing(2),
+    backgroundColor: theme.palette.background.paper,
+    border: '1px solid' + theme.palette.divider,
+  },
+}));
+
+export default function ComponentError({ message }: { message: string }) {
+  const classes = useErrorStyles();
+  return (
+    <Grid
+      className={classes.root}
+      item
+      container
+      direction='column'
+      alignItems='center'
+    >
+      <ErrorOutlineIcon color='error' />
+      <Typography>{message}</Typography>
+    </Grid>
+  );
+}
