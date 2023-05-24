f2be756128b52eea8b00021f4373dcaae6ae78af
Auh3b <ngaiyayec@yahoo.com>
2023-05-24 18:55:33 +0200

    added error boundary components

---
+++ src/components/common/errorHandling/IndicatorBoundary.tsx
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+
+export class IndicatorBoundary extends Component {
+  static propTypes = {}
+
+  render() {
+    return (
+      <div>IndicatorBoundary</div>
+    )
+  }
+}
+
+export default IndicatorBoundary
