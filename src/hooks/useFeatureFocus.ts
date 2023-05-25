f28c71a297a79296cc3c7e9e58c972f64821c44a
Auh3b <ngaiyayec@yahoo.com>
2023-05-25 23:52:54 +0200

    refined Feature focus hook

--- src/hooks/useFeatureFocus.ts
+++ src/hooks/useFeatureFocus.ts
@@ -1,8 +1,50 @@
-import { useDispatch } from "react-redux";
-import useCustomCompareEffect from "./useCustomCompareEffect";
-import { dequal } from "dequal";
+import { useDispatch, useSelector } from 'react-redux';
+import useCustomCompareEffect from './useCustomCompareEffect';
+import { dequal } from 'dequal';
+import { RootState } from 'store/store';
+import { useState } from 'react';
+import getTileFeatures from 'utils/methods/getTileFeatures';
+//@ts-ignore
+import { TILE_FORMATS } from '@deck.gl/carto';
+import { selectAreFeaturesReadyForSource } from '@carto/react-redux';
+import useCustomCompareEffectAlt from 'components/indicators/media/hooks/useCustomCompareEffectAlt';
 
 export default function useFeatureFocus() {
-  const dispatch = useDispatch()
-  useCustomCompareEffect(()=>{}, [dispatch], dequal)
+  const [data, setData] = useState([]);
+  const dispatch = useDispatch();
+  const _sources = useSelector(
+    (state: RootState) => Object.entries(state.carto.dataSources) || [],
+  );
+  // const _isReadys = _sources.map(([source, value])=> useSelector(
+  //   (state) => selectAreFeaturesReadyForSource(state, source),
+  // ))
+  useCustomCompareEffectAlt(
+    () => {
+      //@ts-ignore
+      console.log(_sources);
+      if (_sources.length) {
+        const sourcesPromises = _sources.map(
+          async ([source, { filters, filtersLogicalOperator }]: any) => {
+            const params = {
+              filters,
+              filtersLogicalOperator,
+              tileFormat: TILE_FORMATS,
+              //@ts-ignore
+              limit: null,
+            };
+            //@ts-ignore
+            return getTileFeatures({ sourceId: source, params, global: true });
+          },
+        );
+        Promise.all(sourcesPromises)
+          .then((_data) => {
+            setData(_data);
+          })
+          .catch((error) => console.log(error));
+      }
+    },
+    [dispatch, _sources],
+    dequal,
+  );
+  console.log(data);
 }
