f78ea358cd969a24e18dbcbc57d34f44cb684d24
Auh3b <ngaiyayec@yahoo.com>
2023-05-26 09:56:36 +0200

    refined hook

--- src/hooks/useFeatureFocus.ts
+++ src/hooks/useFeatureFocus.ts
@@ -2,22 +2,34 @@ import { useDispatch, useSelector } from 'react-redux';
 import useCustomCompareEffect from './useCustomCompareEffect';
 import { dequal } from 'dequal';
 import { RootState } from 'store/store';
-import { useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import getTileFeatures from 'utils/methods/getTileFeatures';
 //@ts-ignore
 import { TILE_FORMATS } from '@deck.gl/carto';
-import { selectAreFeaturesReadyForSource } from '@carto/react-redux';
 import useCustomCompareEffectAlt from 'components/indicators/media/hooks/useCustomCompareEffectAlt';
+import { SOURCE_COORD_MAP } from 'data/sources/sourceTypes';
+import { featureCollection, point } from '@turf/helpers';
+import getViewport from 'components/indicators/premise/utils/getViewport';
+import handleMapTransitions from 'components/indicators/premise/utils/handleMapTransitions';
 
 export default function useFeatureFocus() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(',');
   const dispatch = useDispatch();
   const _sources = useSelector(
     (state: RootState) => Object.entries(state.carto.dataSources) || [],
   );
-  // const _isReadys = _sources.map(([source, value])=> useSelector(
-  //   (state) => selectAreFeaturesReadyForSource(state, source),
-  // ))
+  
+  const { width, height } = useSelector(
+    (state: RootState) => state.carto.viewState,
+  );
+  
+  const coordinatesColumns = useMemo(()=>{
+    if(_sources.length){
+      return _sources.map(([name, value])=> SOURCE_COORD_MAP.get(name)|| [])
+    }
+    return []
+  }, [_sources])
+
   useCustomCompareEffectAlt(
     () => {
       //@ts-ignore
@@ -38,12 +50,37 @@ export default function useFeatureFocus() {
         );
         Promise.all(sourcesPromises)
           .then((_data) => {
-            setData(_data);
+            if(_data.length){
+              let _geojson: any[] = []
+              for(let i=0; i < _data.length; i++){
+                const [longitude, latitude] = coordinatesColumns[i]
+                const currentSet = _data[i].map((d:any)=> point([d[longitude], d[latitude]]))
+                _geojson =[..._geojson, ...currentSet] 
+              }
+              const geojson = featureCollection(_geojson)
+              const { latitude, longitude, zoom } = getViewport({
+                geojson,
+                padding: 100,
+                width,
+                height,
+              }); 
+              handleMapTransitions({
+                start: 500,
+                end: 1000,
+                params: {
+                  longitude,
+                  latitude,
+                  zoom,
+                },
+                dispatch,
+              })
+              setData([longitude, latitude, zoom].join('-'));
+            }
           })
           .catch((error) => console.log(error));
       }
     },
-    [dispatch, _sources],
+    [dispatch, coordinatesColumns,_sources, width, height],
     dequal,
   );
   console.log(data);
