ac02cf5ce68c71133128ad9936062d32e06def95
Auh3b <ngaiyayec@yahoo.com>
2023-05-26 09:59:06 +0200

    added word cloud is now provided by echart

--- src/components/indicators/introduction/NnaCountry.tsx
+++ src/components/indicators/introduction/NnaCountry.tsx
@@ -1,20 +1,27 @@
 import { Grid } from '@material-ui/core';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 import groupCategories from '../utils/groupCategories';
 import TitleWrapper from '../../common/TitleWrapper';
 import IntroWordCloud from './utils/IntroWordCloud';
+import CustomWordCloud from 'components/common/customCharts/CustomWordCloud';
+import { useDispatch, useSelector } from 'react-redux';
+import { addIntroFilter, removeIntroFilter } from 'store/introSlice';
+import { _FilterTypes } from '@carto/react-core';
 
 const title = 'Identificación NNA solos';
 const column = 'm07__en_q';
 const subtitle = '';
+const dataSource = 'auroraData'
+const id = 'id'
 
 export default function NnaCountry({
-  data: _data,
+  data: _data = [],
   isLoading,
 }: {
   data: any[];
   isLoading: Boolean;
 }) {
+  const dispatch = useDispatch()
   const data = useMemo(() => {
     if (_data) {
       const category = groupCategories(_data, column);
@@ -22,11 +29,41 @@ export default function NnaCountry({
       return category;
     }
   }, [_data]);
+//@ts-ignore
+  const filters = useSelector(state => state.intro.filters[dataSource]) || {}
+
+  const onWordSelectChange = useCallback(
+    ({ value }: any, selectedWord) => {
+      console.log(selectedWord)
+      const [x, y, text, ...rest] = value;
+      if (selectedWord === text) {
+        dispatch(
+          removeIntroFilter({
+            owner: id,
+            source: dataSource,
+            column: column,
+          }),
+        );
+      } else {
+        dispatch(
+          addIntroFilter({
+            owner: id,
+            source: dataSource,
+            values: [text],
+            column: column,
+            type: _FilterTypes.IN,
+          }),
+        );
+      }
+    }
+    ,[data, dispatch, filters]
+  )
 
   return (
     <TitleWrapper title={title} subtitle={subtitle} isLoading={isLoading}>
       <Grid item>
-        <IntroWordCloud data={data} />
+        <CustomWordCloud data={data} id={id} filters={filters} onWordSelectChange={onWordSelectChange}/>
+        {/* <IntroWordCloud data={data} /> */}
       </Grid>
     </TitleWrapper>
   );
