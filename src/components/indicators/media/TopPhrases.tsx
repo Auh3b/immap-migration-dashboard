571d9587af1f42386322d48cf8c59afaa498d876
Auh3b <ngaiyayec@yahoo.com>
2023-05-26 10:17:10 +0200

    added filter function

--- src/components/indicators/media/TopPhrases.tsx
+++ src/components/indicators/media/TopPhrases.tsx
@@ -3,8 +3,11 @@ import TitleWrapper from 'components/common/TitleWrapper';
 import CustomWordCloud from 'components/common/customCharts/CustomWordCloud';
 import { METHOD_NAMES } from 'components/views/mediaViews/utils/methodName';
 import useMediaData from './hooks/useMediaData';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ContinuousLegend from 'components/common/customCharts/ContinuousLegend';
+import { useCallback } from 'react';
+import { addMediaFilter, removeMediaFilter } from 'store/mediaSlice';
+import { FilterTypes } from 'utils/filterFunctions';
 
 const id = 'top_phrases';
 
@@ -13,19 +16,46 @@ const useStyles = makeStyles((theme) => ({
     position: 'relative',
   },
 }));
+const dataSource = 'meltwater'
+const column = 'topPhrases'
 
 export default function TopPhrases() {
   const { data, isLoading } = useMediaData({
     id,
     methodName: METHOD_NAMES.MEDIA_TOP_PHRASES,
   });
+  const dispatch = useDispatch()
   //@ts-ignore
   const filters = useSelector((state) => state.media.filters?.meltwater) || {};
   const classes = useStyles();
+  const onWordSelectChange = useCallback(
+        ({ value }: any, selectedWord: string) => {
+      const [x, y, text, ...rest] = value;
+      if (selectedWord === text) {
+        dispatch(
+          removeMediaFilter({
+            owner: id,
+            source: dataSource,
+            column,
+          }),
+        );
+      } else {
+        dispatch(
+          addMediaFilter({
+            owner: id,
+            source: dataSource,
+            values: [text],
+            column,
+            type: FilterTypes.WORD_CLOUD_IN,
+          }),
+        );
+      }
+    },[dispatch, data]
+  )
   return (
     <Grid xs={12} lg={4} item className={classes.root}>
       <TitleWrapper title='Palabras asociadas' isLoading={isLoading}>
-        <CustomWordCloud data={data} filters={filters} id={id} />
+        <CustomWordCloud onWordSelectChange={onWordSelectChange} data={data} filters={filters} id={id} />
         <ContinuousLegend colorScheme={['#fd8d3c', '#800026']} />
       </TitleWrapper>
     </Grid>
