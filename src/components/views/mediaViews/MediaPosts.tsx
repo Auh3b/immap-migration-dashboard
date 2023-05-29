93527040bf9fb4e397f91a2f9b4ded89c0a477d2
Auh3b <ngaiyayec@yahoo.com>
2023-05-29 15:49:21 +0200

    change data grid to regular table

--- src/components/views/mediaViews/MediaPosts.tsx
+++ src/components/views/mediaViews/MediaPosts.tsx
@@ -1,15 +1,16 @@
-import { Button, Fade, Grid, IconButton, Paper, Popper, Typography, makeStyles } from '@material-ui/core';
+import {
+  Grid,
+  Paper,
+  makeStyles,
+} from '@material-ui/core';
 import useMediaData from 'components/indicators/media/hooks/useMediaData';
 import { METHOD_NAMES } from './utils/methodName';
 import TopLoading from 'components/common/TopLoading';
 import ComponentFallback from 'components/common/ComponentFallback';
-import SocialPost from 'components/common/media/SocialPost';
-import { MouseEvent, useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import MediaTable from 'components/common/media/MediaTable';
 import { numberFormatter } from 'utils/formatter';
 import NoWidgetData from 'components/common/customWidgets/NoWidgetData';
-import LinkIcon from '@material-ui/icons/Link';
-import VisibilityIcon from '@material-ui/icons/Visibility';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -63,9 +64,6 @@ export default function MediaPosts({ isLoading }: { isLoading: Boolean }) {
       headerName: 'URL/ID',
       sort: true,
       width: 120,
-      renderCell: (params:any) =>(
-          <ViewActionButtons params={params}/>
-        )
     },
     {
       field: 'name',
@@ -102,59 +100,9 @@ export default function MediaPosts({ isLoading }: { isLoading: Boolean }) {
                 columnConfig={columnConfig}
               />
             ))}
-          {(!_data.length && !isLoading) && <NoWidgetData />}
+          {!_data.length && !isLoading && <NoWidgetData />}
         </Grid>
       </Paper>
     </Grid>
   );
-}
-
-const useLinkStyles = makeStyles((theme)=>({
-  root:{
-    gap: theme.spacing(1),
-  },
-  button: {
-    // borderRadius: '100%',
-  },
-  popper:{
-    zIndex: theme.zIndex.appBar
-  },
-  paper:{
-    padding: theme.spacing(2)
-  }
-}))
-
-function ViewActionButtons({params}:any){
-  const { value: url, row: {source} } = params
-  const classes = useLinkStyles()
-  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
-  const open = Boolean(anchorEl)
-
-  const handleToggle = (event: MouseEvent<HTMLElement>) => {
-    setAnchorEl(anchorEl ? null : event.currentTarget);
-  };
-
-  console.log(url, anchorEl, open)
-  return (
-    <Grid container alignItems='center' className={classes.root}>
-      {/* <IconButton color='primary' onClick={handleToggle} className={classes.button}>
-        <VisibilityIcon />
-      </IconButton> */}
-      <Button startIcon={<LinkIcon />} component={'a'} onClick={handleToggle} href={url} target='blank' className={classes.button}>
-        <Typography noWrap>
-          {url}
-        </Typography>
-      </Button>
-      {/* <Popper anchorEl={anchorEl} placement='top-end' open={open} className={classes.popper}>
-        {({TransitionProps})=>(
-          <Fade {...TransitionProps}>
-            <Paper elevation={0} className={classes.paper}>
-              Content
-              <SocialPost source={source} url={url} />
-            </Paper>
-          </Fade>
-        )}
-      </Popper> */}
-    </Grid>
-  )
 }
