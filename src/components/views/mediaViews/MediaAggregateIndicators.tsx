a4b1acba47aaea085bbeef3d2d378b0e1cc8e7e3
Auh3b <ngaiyayec@yahoo.com>
2023-06-02 16:51:36 +0200

    changed filtering

--- src/components/views/mediaViews/MediaAggregateIndicators.tsx
+++ src/components/views/mediaViews/MediaAggregateIndicators.tsx
@@ -4,12 +4,13 @@ import {
   Typography,
   makeStyles,
   useTheme,
+  withStyles,
 } from '@material-ui/core';
 import SourceIndictor from 'components/indicators/media/utils/SourceIndictor';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { MouseEvent, useMemo } from 'react';
 import ComponentFallback from 'components/common/ComponentFallback';
-import { FA_MAP, MEDIA_SOURCES } from './utils/mediaUtils';
+import { FA_MAP, MEDIA_SOURCES, SOURCE_COLOR } from './utils/mediaUtils';
 import { METHOD_NAMES } from './utils/methodName';
 import { ToggleButton, ToggleButtonGroup } from '@material-ui/lab';
 import { addMediaFilter, removeMediaFilter } from 'store/mediaSlice';
@@ -18,6 +19,9 @@ import { _FilterTypes } from '@carto/react-core';
 import useMediaData from 'components/indicators/media/hooks/useMediaData';
 import TopLoading from 'components/common/TopLoading';
 import getSourceFilter from 'components/indicators/media/utils/getSourceFilter';
+import { UNICEF_COLORS } from 'theme';
+
+const id = 'menciones_sociales';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,15 +38,33 @@ const useStyles = makeStyles((theme) => ({
   indicatorContainer: {
     height: '100%',
     padding: theme.spacing(2),
-  },
-  indicatorButton: {
-    height: '100%',
-    width: '100%',
+    gap: theme.spacing(0.5),
   },
   icon: {},
 }));
 
-const id = 'menciones_sociales';
+const StyleToggleButtonGroup = withStyles((theme) => ({
+  root: {},
+  groupedHorizontal: {
+    '&:not(:first-child)': {
+      borderLeft: 'unset',
+      marginLeft: 'unset',
+    },
+  },
+}))(ToggleButtonGroup);
+
+const StyledToggleButton = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(0.5),
+    margin: theme.spacing(0.5),
+    height: '100%',
+    width: '100%',
+  },
+  selected: {
+    backgroundColor: 'rgba(0,0,0,0)',
+    border: '3px solid' + UNICEF_COLORS[0] + ' !important',
+  },
+}))(ToggleButton);
 
 export default function MediaAggregateIndicators({ isLoading }: any) {
   const dispatch = useDispatch();
@@ -109,7 +131,8 @@ export default function MediaAggregateIndicators({ isLoading }: any) {
           Número de menciones por red social relacionadas con migración en la
           región
         </Typography>
-        <ToggleButtonGroup
+        {/* @ts-expect-error */}
+        <StyleToggleButtonGroup
           exclusive
           onChange={handleSourceChange}
           value={source}
@@ -118,11 +141,8 @@ export default function MediaAggregateIndicators({ isLoading }: any) {
           {data.length > 0 &&
             !isLoading &&
             data.map(({ name, value }, i) => (
-              <ToggleButton
-                key={name}
-                value={name}
-                className={classes.indicatorButton}
-              >
+              //@ts-expect-error
+              <StyledToggleButton key={name} value={name}>
                 <SourceIndictor
                   title={name}
                   icon={
@@ -130,15 +150,20 @@ export default function MediaAggregateIndicators({ isLoading }: any) {
                     <FontAwesomeIcon
                       className={classes.icon}
                       icon={FA_MAP.get(name)}
-                      style={{ color: theme.palette.grey[100] }}
+                      style={{
+                        color:
+                          source === name
+                            ? SOURCE_COLOR[name]
+                            : theme.palette.grey[100],
+                      }}
                     />
                   }
                   data={value}
                 />
-              </ToggleButton>
+              </StyledToggleButton>
             ))}
           {isLoading && <ComponentFallback />}
-        </ToggleButtonGroup>
+        </StyleToggleButtonGroup>
       </Paper>
     </Grid>
   );
