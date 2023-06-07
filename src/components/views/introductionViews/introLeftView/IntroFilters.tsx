d4f29225bcf32b9ba0d3592c90c4ef1275ee3740
Auh3b <ngaiyayec@yahoo.com>
2023-06-07 18:33:41 +0200

    remove filter item in component

--- src/components/views/introductionViews/introLeftView/IntroFilters.tsx
+++ src/components/views/introductionViews/introLeftView/IntroFilters.tsx
@@ -2,37 +2,24 @@ import { Grid, Typography } from '@material-ui/core';
 import ActiveFilterItem, {
   ActiveFilterItemProps,
 } from 'components/common/ActiveFilterItem';
-import { useDispatch, useSelector } from 'react-redux';
-import { removeIntroFilter } from 'store/introSlice';
+import { useSelector } from 'react-redux';
 
 type Filters = Record<string, Record<string, ActiveFilterItemProps>>;
 
 export default function IntroFilters() {
-  const dispatch = useDispatch();
   // @ts-ignore
   const filters: Filters = useSelector((state) => state.intro.filters);
-  const handleRemove = (props: Partial<ActiveFilterItemProps>) => {
-    const { id: owner, source, column } = props;
-    dispatch(
-      removeIntroFilter({
-        source,
-        column,
-        owner,
-      }),
-    );
-  };
   return (
     <Grid container direction='column'>
       {Object.entries(filters).map(([name, _filters]) => (
         <div key={name}>
-          <Typography>{name}</Typography>
+          <Typography variant='subtitle2' style={{textTransform: 'uppercase'}}>{name+ ' data'}</Typography>
           {Object.entries(_filters).map(([filterName, _filterItem]) => (
             <ActiveFilterItem
               key={filterName}
               {..._filterItem}
               id={name}
               name={filterName}
-              removeItem={handleRemove}
             />
           ))}
         </div>
