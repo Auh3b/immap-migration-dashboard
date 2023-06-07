8b57e1aac2691c6632d8b320dad3e0144682dad2
Auh3b <ngaiyayec@yahoo.com>
2023-06-07 18:32:56 +0200

    changed formula

--- src/components/indicators/introduction/ChildrenPercentage.tsx
+++ src/components/indicators/introduction/ChildrenPercentage.tsx
@@ -8,22 +8,14 @@ import { Grid } from '@material-ui/core';
 import { EXTERNAL_METHOD_NAMES } from 'utils/methods/methods';
 import useIntroData from './hooks/useIntroData';
 
-const id = 'childrenPercentage';
+const id = 'children_percentage';
 const title = 'Porcentaje NNA en grupos de viaje';
 const column = '';
 const methodName = EXTERNAL_METHOD_NAMES.AGGREGATE_COLUMNS;
 const source = 'aurora';
 const columns = [
   {
-    name: 'e20__cua',
-    type: SummarisationTypes.SUM,
-  },
-  {
-    name: 'e21__cua',
-    type: SummarisationTypes.SUM,
-  },
-  {
-    name: 'e22__cua',
+    name: 'e19_cu',
     type: SummarisationTypes.SUM,
   },
   {
@@ -47,7 +39,7 @@ export default function ChildrenPercentage() {
   const data = useMemo(() => {
     if (_data.length) {
       return (
-        (_data[0].value + _data[1].value + _data[2].value) / _data[3].value
+        _data[0].value / _data[1].value
       );
     }
     return 0;
