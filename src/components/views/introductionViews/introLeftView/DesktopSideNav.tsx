32bef43c5b47dfc5df740eee38566ea397c4bd60
Auh3b <ngaiyayec@yahoo.com>
2023-06-14 00:27:42 +0200

    re-instate old panel version

--- src/components/views/introductionViews/introLeftView/DesktopSideNav.tsx
+++ src/components/views/introductionViews/introLeftView/DesktopSideNav.tsx
@@ -20,225 +20,225 @@ import { UNICEF_COLORS } from 'theme';
 import { dequal } from 'dequal';
 import SideAnalyticsPanel from 'components/common/sideAnalysticsPanel/Index';
 
-export default function DesktopSideNav() {
-  return <SideAnalyticsPanel filterSources={[{ stateSlice: 'intro' }]} />;
+// export default function DesktopSideNav() {
+//   return <SideAnalyticsPanel filterSources={[{ stateSlice: 'intro' }]} />;
+// }
+
+const drawerWidth = 300;
+
+export const useStyles = makeStyles((theme) => ({
+  drawer: {
+    width: drawerWidth,
+  },
+  drawerOpen: {
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerClose: {
+    width: theme.mixins.toolbar.minHeight,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  },
+  drawerPadding: {
+    height: theme.spacing(2),
+  },
+  buttonContainer: {
+    borderRadius: '100%',
+    marginTop: theme.mixins.toolbar.minHeight,
+    borderBottom: '1px solid' + theme.palette.divider,
+    padding: theme.spacing(1),
+    display: 'flex',
+    justifyContent: 'flex-end',
+  },
+}));
+
+export default function DesktopSideNav({ isOpen, handleOpenToggle }: any) {
+  const [value, setValue] = useState(0);
+  const classes = useStyles({ isOpen });
+  useEffect(() => {
+    if (value) {
+      handleOpenToggle(true);
+    } else {
+      handleOpenToggle(false);
+    }
+  }, [value, setValue]);
+  return (
+    <Drawer
+      variant='permanent'
+      anchor='left'
+      className={clsx(classes.drawer, {
+        [classes.drawerOpen]: isOpen,
+        [classes.drawerClose]: !isOpen,
+      })}
+      classes={{
+        paper: clsx({
+          [classes.drawerOpen]: isOpen,
+          [classes.drawerClose]: !isOpen,
+        }),
+      }}
+      open={isOpen}
+    >
+      <Grid
+        wrap='nowrap'
+        container
+        alignItems='stretch'
+        style={{ width: '100%', height: '100%', paddingTop: '56px' }}
+      >
+        <ContentPanel value={value} />
+        {isOpen && <Divider orientation='vertical' />}
+        <SideMenu isOpen={isOpen} setValue={setValue} value={value} />
+      </Grid>
+    </Drawer>
+  );
 }
 
-// const drawerWidth = 300;
+const useNavStyle = makeStyles((theme) => ({
+  root: {
+    width: theme.mixins.toolbar.minHeight,
+    color: ({ value, isCurrent }: any) =>
+      isCurrent ? UNICEF_COLORS[0] : value ? grey[400] : red[400],
+  },
+}));
 
-// export const useStyles = makeStyles((theme) => ({
-//   drawer: {
-//     width: drawerWidth,
-//   },
-//   drawerOpen: {
-//     width: drawerWidth,
-//     transition: theme.transitions.create('width', {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   },
-//   drawerClose: {
-//     width: theme.mixins.toolbar.minHeight,
-//     transition: theme.transitions.create('width', {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//   },
-//   drawerPadding: {
-//     height: theme.spacing(2),
-//   },
-//   buttonContainer: {
-//     borderRadius: '100%',
-//     marginTop: theme.mixins.toolbar.minHeight,
-//     borderBottom: '1px solid' + theme.palette.divider,
-//     padding: theme.spacing(1),
-//     display: 'flex',
-//     justifyContent: 'flex-end',
-//   },
-// }));
+function NavButton({
+  value,
+  selectedValue,
+  title,
+  onValueChange,
+  icon,
+}: {
+  value: number;
+  selectedValue: number;
+  title: string;
+  onValueChange: (e: MouseEvent, value: number) => void;
+  icon: ReactNode;
+}) {
+  const isCurrent = dequal(value, selectedValue);
+  const classes = useNavStyle({ value, isCurrent });
+  return (
+    <Tooltip title={title} placement='right'>
+      <IconButton
+        onClick={(e) => onValueChange(e, value)}
+        className={classes.root}
+      >
+        {icon}
+      </IconButton>
+    </Tooltip>
+  );
+}
 
-// export default function DesktopSideNav({ isOpen, handleOpenToggle }: any) {
-//   const [value, setValue] = useState(0);
-//   const classes = useStyles({ isOpen });
-//   useEffect(() => {
-//     if (value) {
-//       handleOpenToggle(true);
-//     } else {
-//       handleOpenToggle(false);
-//     }
-//   }, [value, setValue]);
-//   return (
-//     <Drawer
-//       variant='permanent'
-//       anchor='left'
-//       className={clsx(classes.drawer, {
-//         [classes.drawerOpen]: isOpen,
-//         [classes.drawerClose]: !isOpen,
-//       })}
-//       classes={{
-//         paper: clsx({
-//           [classes.drawerOpen]: isOpen,
-//           [classes.drawerClose]: !isOpen,
-//         }),
-//       }}
-//       open={isOpen}
-//     >
-//       <Grid
-//         wrap='nowrap'
-//         container
-//         alignItems='stretch'
-//         style={{ width: '100%', height: '100%', paddingTop: '56px' }}
-//       >
-//         <ContentPanel value={value} />
-//         {isOpen && <Divider orientation='vertical' />}
-//         <SideMenu isOpen={isOpen} setValue={setValue} value={value} />
-//       </Grid>
-//     </Drawer>
-//   );
-// }
+function SideMenu({
+  isOpen,
+  value,
+  setValue,
+}: {
+  isOpen: Boolean;
+  value: number;
+  setValue: (value: number) => void;
+}) {
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue);
+  };
 
-// const useNavStyle = makeStyles((theme) => ({
-//   root: {
-//     width: theme.mixins.toolbar.minHeight,
-//     color: ({ value, isCurrent }: any) =>
-//       isCurrent ? UNICEF_COLORS[0] : value ? grey[400] : red[400],
-//   },
-// }));
+  const values: Record<number, string> = {
+    0: 'Cerrar',
+    1: 'Metodología',
+    2: 'Filtros Adicionales',
+    3: 'Filtros Activos',
+  };
 
-// function NavButton({
-//   value,
-//   selectedValue,
-//   title,
-//   onValueChange,
-//   icon,
-// }: {
-//   value: number;
-//   selectedValue: number;
-//   title: string;
-//   onValueChange: (e: MouseEvent, value: number) => void;
-//   icon: ReactNode;
-// }) {
-//   const isCurrent = dequal(value, selectedValue);
-//   const classes = useNavStyle({ value, isCurrent });
-//   return (
-//     <Tooltip title={title} placement='right'>
-//       <IconButton
-//         onClick={(e) => onValueChange(e, value)}
-//         className={classes.root}
-//       >
-//         {icon}
-//       </IconButton>
-//     </Tooltip>
-//   );
-// }
+  return (
+    <Grid
+      direction='column'
+      alignItems='flex-start'
+      item
+      xs
+      container
+      style={{ color: grey['600'] }}
+    >
+      <NavButton
+        value={1}
+        selectedValue={value}
+        title={values[1]}
+        onValueChange={handleChange}
+        icon={<HelpOutlineIcon />}
+      />
+      <NavButton
+        value={2}
+        selectedValue={value}
+        title={values[2]}
+        onValueChange={handleChange}
+        icon={<FilterListIcon />}
+      />
+      <NavButton
+        value={3}
+        selectedValue={value}
+        title={values[3]}
+        onValueChange={handleChange}
+        icon={<TuneIcon />}
+      />
+      {value ? (
+        <NavButton
+          value={0}
+          selectedValue={value}
+          title={values[0]}
+          onValueChange={handleChange}
+          icon={<ChevronLeftIcon />}
+        />
+      ) : (
+        <></>
+      )}
+    </Grid>
+  );
+}
 
-// function SideMenu({
-//   isOpen,
-//   value,
-//   setValue,
-// }: {
-//   isOpen: Boolean;
-//   value: number;
-//   setValue: (value: number) => void;
-// }) {
-//   const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-//     setValue(newValue);
-//   };
+function TabPanel({
+  value,
+  index,
+  children,
+}: {
+  value: any;
+  index: any;
+  children: ReactNode;
+}) {
+  return (
+    <div
+      role='tabpanel'
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      style={{ padding: '8px 0px 8px 24px' }}
+    >
+      {children}
+    </div>
+  );
+}
 
-//   const values: Record<number, string> = {
-//     0: 'Cerrar',
-//     1: 'Metodología',
-//     2: 'Filtros Adicionales',
-//     3: 'Filtros Activos',
-//   };
+const usePanelStyles = makeStyles((theme) => ({
+  root: {
+    width: ({ value }: any) => (value ? '100%' : '0%'),
+    flexGrow: ({ value }: any) => (value ? 1 : 0),
+  },
+}));
 
-//   return (
-//     <Grid
-//       direction='column'
-//       alignItems='flex-start'
-//       item
-//       xs
-//       container
-//       style={{ color: grey['600'] }}
-//     >
-//       <NavButton
-//         value={1}
-//         selectedValue={value}
-//         title={values[1]}
-//         onValueChange={handleChange}
-//         icon={<HelpOutlineIcon />}
-//       />
-//       <NavButton
-//         value={2}
-//         selectedValue={value}
-//         title={values[2]}
-//         onValueChange={handleChange}
-//         icon={<FilterListIcon />}
-//       />
-//       <NavButton
-//         value={3}
-//         selectedValue={value}
-//         title={values[3]}
-//         onValueChange={handleChange}
-//         icon={<TuneIcon />}
-//       />
-//       {value ? (
-//         <NavButton
-//           value={0}
-//           selectedValue={value}
-//           title={values[0]}
-//           onValueChange={handleChange}
-//           icon={<ChevronLeftIcon />}
-//         />
-//       ) : (
-//         <></>
-//       )}
-//     </Grid>
-//   );
-// }
-
-// function TabPanel({
-//   value,
-//   index,
-//   children,
-// }: {
-//   value: any;
-//   index: any;
-//   children: ReactNode;
-// }) {
-//   return (
-//     <div
-//       role='tabpanel'
-//       hidden={value !== index}
-//       id={`simple-tabpanel-${index}`}
-//       aria-labelledby={`simple-tab-${index}`}
-//       style={{ padding: '8px 0px 8px 24px' }}
-//     >
-//       {children}
-//     </div>
-//   );
-// }
-
-// const usePanelStyles = makeStyles((theme) => ({
-//   root: {
-//     width: ({ value }: any) => (value ? '100%' : '0%'),
-//     flexGrow: ({ value }: any) => (value ? 1 : 0),
-//   },
-// }));
-
-// function ContentPanel({ value }: any) {
-//   const classes = usePanelStyles({ value });
-//   return (
-//     <Grid item className={classes.root}>
-//       <TabPanel value={value} index={1}>
-//         <IntroContent isOpen={true} />
-//       </TabPanel>
-//       <TabPanel value={value} index={2}>
-//         <IntroExtraFilters />
-//       </TabPanel>
-//       <TabPanel value={value} index={3}>
-//         <IntroActiveFilters />
-//       </TabPanel>
-//     </Grid>
-//   );
-// }
+function ContentPanel({ value }: any) {
+  const classes = usePanelStyles({ value });
+  return (
+    <Grid item className={classes.root}>
+      <TabPanel value={value} index={1}>
+        <IntroContent isOpen={true} />
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        <IntroExtraFilters />
+      </TabPanel>
+      <TabPanel value={value} index={3}>
+        <IntroActiveFilters />
+      </TabPanel>
+    </Grid>
+  );
+}
