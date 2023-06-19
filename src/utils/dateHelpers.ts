5418164a27b9d6a7ececeda1787868076095ce61
Auh3b <ngaiyayec@yahoo.com>
2023-06-19 17:13:30 +0200

    added function to check if data is within interval

--- src/utils/dateHelpers.ts
+++ src/utils/dateHelpers.ts
@@ -19,14 +19,20 @@ import {
   getUnixTime,
   getWeek,
   getYear,
+  isWithinInterval,
+  startOfDay,
+  startOfMonth,
+  startOfQuarter,
+  startOfWeek,
+  startOfYear,
 } from 'date-fns';
 
 export function getUnixTimestamp(date: Date) {
-  return getUnixTime(date);
+  return getUnixTime(date) * 1000;
 }
 
 export function fromUnixTimestamp(value: number) {
-  return fromUnixTime(value);
+  return fromUnixTime(value / 1000);
 }
 
 enum intervalsPeriod {
@@ -39,7 +45,16 @@ enum intervalsPeriod {
 
 type IntervalLookFunction = Record<intervalsPeriod, Function>;
 
+const intervalStartFunction: IntervalLookFunction = Object.fromEntries([
+  [intervalsPeriod.DAY, startOfDay],
+  [intervalsPeriod.WEEK, startOfWeek],
+  [intervalsPeriod.MONTH, startOfMonth],
+  [intervalsPeriod.QUARTER, startOfQuarter],
+  [intervalsPeriod.YEAR, startOfYear],
+]);
+
 const intervalPeriodFunction: IntervalLookFunction = Object.fromEntries([
+  [intervalsPeriod.DAY, endOfDay],
   [intervalsPeriod.WEEK, endOfWeek],
   [intervalsPeriod.MONTH, endOfMonth],
   [intervalsPeriod.QUARTER, endOfQuarter],
@@ -55,7 +70,7 @@ const intervalValueFunction: IntervalLookFunction = Object.fromEntries([
 ]);
 
 const intervalNameFunction: IntervalLookFunction = Object.fromEntries([
-  [intervalsPeriod.DAY, (date: Date) => format(date, 'ee')],
+  [intervalsPeriod.DAY, (date: Date) => format(date, 'dd')],
   [intervalsPeriod.WEEK, (date: Date) => format(date, 'ww')],
   [intervalsPeriod.MONTH, (date: Date) => format(date, 'MMM')],
   [intervalsPeriod.QUARTER, (date: Date) => format(date, 'QQQ')],
@@ -69,11 +84,21 @@ export function getInterval(date: Date, interval: intervalsPeriod) {
   const end = getUnixTimestamp(_end);
   const intervalValue: number = intervalValueFunction[interval](date);
   const name: string = intervalNameFunction[interval](date);
+  const intervalCategory = interval;
   return {
     name,
     intervalValue,
+    intervalCategory,
     start,
     end,
+  };
+}
+
+function isWithTargetInterval(target: { start: Date; end: Date }) {
+  return (date: Date, intervalType: intervalsPeriod) => {
+    const start = intervalStartFunction[intervalType](target.start);
+    const end = intervalStartFunction[intervalType](target.end);
+    return isWithinInterval(date, { start, end });
   };
 }
 
@@ -85,6 +110,10 @@ export function getTemporalFilters(
   const dates = defaultFilterFunction(input, column).map((d) => d[column]);
   const output: Record<string, any> = {};
   const [start, end] = extent(dates.map((d) => +d));
+  const getIsWithInterval = isWithTargetInterval({
+    start: fromUnixTimestamp(start),
+    end: fromUnixTimestamp(end),
+  });
   const years = eachYearOfInterval({
     start,
     end,
@@ -99,8 +128,9 @@ export function getTemporalFilters(
     });
 
     for (let quarter of quarters) {
-      let _quarterOutput = getInterval(quarter, intervalsPeriod.QUARTER);
+      if (!getIsWithInterval(quarter, intervalsPeriod.QUARTER)) continue;
 
+      let _quarterOutput = getInterval(quarter, intervalsPeriod.QUARTER);
       let months = eachMonthOfInterval({
         start: fromUnixTimestamp(_quarterOutput.start),
         end: fromUnixTimestamp(_quarterOutput.end),
@@ -108,39 +138,57 @@ export function getTemporalFilters(
       let quarterChildren: any[] = [];
 
       for (let month of months) {
+        if (!getIsWithInterval(month, intervalsPeriod.MONTH)) continue;
         let _monthOutput = getInterval(month, intervalsPeriod.MONTH);
         let weeks = eachWeekOfInterval({
           start: fromUnixTimestamp(_monthOutput.start),
           end: fromUnixTimestamp(_monthOutput.end),
         });
         let monthChildren: any[] = [];
-        for (let week of weeks) {
-          let _weekOutput = getInterval(week, intervalsPeriod.WEEK);
-          let days = eachDayOfInterval({
-            start: fromUnixTimestamp(_weekOutput.start),
-            end: fromUnixTimestamp(_weekOutput.end),
-          }).map((d) => ({
-            name: intervalNameFunction[intervalsPeriod.DAY](d),
-            start: getUnixTimestamp(d),
-            end: getUnixTimestamp(endOfDay(d)),
-          }));
+        for (let i = 0; i < weeks.length; i++) {
+          if (!getIsWithInterval(weeks[i], intervalsPeriod.WEEK)) continue;
+          let _weekOutput = getInterval(weeks[i], intervalsPeriod.WEEK);
+          // let days = eachDayOfInterval({
+          //   start: fromUnixTimestamp(_weekOutput.start),
+          //   end: fromUnixTimestamp(_weekOutput.end),
+          // }).map((d) => [
+          //   intervalNameFunction[intervalsPeriod.DAY](d),
+          //   getInterval(d, intervalsPeriod.DAY),
+          // ]);
+
           monthChildren = [
             ...monthChildren,
-            { ..._weekOutput, children: days },
+            [
+              `WK${i + 1}`,
+              {
+                ..._weekOutput,
+                name: `WK${i + 1}`,
+                // children: Object.fromEntries(days),
+              },
+            ],
           ];
         }
         quarterChildren = [
           ...quarterChildren,
-          { ..._monthOutput, children: monthChildren },
+          [
+            _monthOutput.name,
+            { ..._monthOutput, children: Object.fromEntries(monthChildren) },
+          ],
         ];
       }
       yearChildren = [
         ...yearChildren,
-        { ..._quarterOutput, children: quarterChildren },
+        [
+          _quarterOutput.name,
+          { ..._quarterOutput, children: Object.fromEntries(quarterChildren) },
+        ],
       ];
     }
 
-    output[_yearOutput.name] = { ..._yearOutput, children: yearChildren };
+    output[_yearOutput.name] = {
+      ..._yearOutput,
+      children: Object.fromEntries(yearChildren),
+    };
   }
 
   return output;
