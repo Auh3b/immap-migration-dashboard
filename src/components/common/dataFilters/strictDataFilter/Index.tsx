5570a478a4ec0bddecde4036c587bcb4e0f36bab
Auh3b <ngaiyayec@yahoo.com>
2023-06-19 17:53:29 +0200

    change data retrieval and state management

--- src/components/common/dataFilters/strictDataFilter/Index.tsx
+++ src/components/common/dataFilters/strictDataFilter/Index.tsx
@@ -1,8 +1,11 @@
-import { MouseEvent, useState } from 'react';
+import { MouseEvent, useCallback, useEffect, useState } from 'react';
 import CustomTab from './utils/CustomTab';
-import useIntroData from 'components/indicators/introduction/hooks/useIntroData';
 import { EXTERNAL_METHOD_NAMES } from 'utils/methods/methods';
 import { FilterTypes } from 'utils/filterFunctions';
+import { useDispatch, useSelector } from 'react-redux';
+import executeIntroMethod from 'components/indicators/introduction/utils/executeIntroMethod';
+import { addIntroFilter } from 'store/introSlice';
+import { removeIntroFilter } from 'store/introSlice';
 
 const source = 'aurora';
 const id = 'fecha_filtro';
@@ -11,24 +14,60 @@ const type = FilterTypes.BETWEEN;
 const methodName = EXTERNAL_METHOD_NAMES.GET_TEMPORAL_FILTER_VALUES;
 
 export default function StrictDateFilter() {
+  const dispatch = useDispatch()
+  //@ts-ignore
+  const isIntroDataReady = useSelector((state) => state.intro.isIntroDataReady)
+  const [data, setData] = useState<{} | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
   const [selected, setSelected] = useState<string | null>(null);
-  const onSelectionChange = (
+  
+  const onSelectionChange = useCallback((
     event: MouseEvent<HTMLElement>,
     newValue: string,
   ) => {
-    setSelected(newValue);
-  };
+    if(newValue){
+      setSelected(newValue);
+      //@ts-ignore
+      const selectedData = data[newValue]
+      const values = [selectedData.start, selectedData.end]
+      dispatch(
+        addIntroFilter({
+            owner: id,
+            column,
+            source,
+            type,
+            values: [values],
+        })
+      )
+    }else{
+      setSelected(null)
+      dispatch(
+        removeIntroFilter({
+            owner: id,
+            column,
+            source,
+        })
+      )
+    }
+  }, [data, isIntroDataReady, dispatch,selected]);
 
-  const { data, isLoading } = useIntroData({
-    id,
-    source,
-    column,
-    methodName,
-  });
+  useEffect(()=>{
+    if(isIntroDataReady){
+      executeIntroMethod({
+        source,
+        methodName,
+        column
+      }).then((data) => setData(data)).finally(()=> setIsLoading(false))
+    }
+    return () =>{
+      setData(null)
+      setIsLoading(false)
+    }
+  }, [isIntroDataReady])
 
   return (
     <>
-      {Object.keys(data).length && !isLoading ? (
+      {data && !isLoading ? (
         <CustomTab
           id={id}
           column={column}
