5027dac29d682392351936b54e82af6a40db28da
Auh3b <ngaiyayec@yahoo.com>
2023-10-24 22:10:53 +0200

    added png org logs

--- src/components/common/header/AppLogos.tsx
+++ src/components/common/header/AppLogos.tsx
@@ -1,9 +1,10 @@
 import { ReactComponent as UnicefLogo } from 'assets/img/unice-logo-1-sp.svg';
-
+import OrgLogo from 'assets/img/3iSolution_tagless.png';
 export default function AppLogos() {
   return (
     <>
       <UnicefLogo />
+      <img src={OrgLogo} style={{ height: '24px' }} />
     </>
   );
 }
