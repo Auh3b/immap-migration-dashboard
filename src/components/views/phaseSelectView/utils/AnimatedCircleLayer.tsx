6fd7cb2a6a6f08f18c0632ec08f818240dcb191d
Auh3b <ngaiyayec@yahoo.com>
2023-12-18 17:01:34 +0200

    added text props

--- src/components/views/phaseSelectView/utils/AnimatedCircleLayer.tsx
+++ src/components/views/phaseSelectView/utils/AnimatedCircleLayer.tsx
@@ -1,6 +1,26 @@
 import { GeoJsonLayer } from '@deck.gl/layers';
 import { CompositeLayer } from 'deck.gl';
 
+const generateText = (
+  d: Record<string, unknown>,
+  keyMap: Record<string, string>,
+) => {
+  const object_array = Object.entries(d);
+  const keys = Object.keys(keyMap);
+  const filtered_array = object_array.filter(([key, value]) =>
+    keys.includes(key),
+  );
+  const text_map = filtered_array
+    .map(([key, value]) => `${keyMap[key] || key}: ${value}`)
+    .join('\n');
+  console.log(object_array, keys, filtered_array, text_map);
+  return text_map;
+};
+
+const keys = {
+  aggregated_count: 'Count',
+};
+
 class AnimatedCircleLayer extends CompositeLayer<any, any> {
   constructor(props: any) {
     super(props);
@@ -16,6 +36,7 @@ class AnimatedCircleLayer extends CompositeLayer<any, any> {
           data,
           pointType: 'circle',
           stroked: false,
+          pickable: true,
           pointRadiusScale: 1,
           pointRadiusUnits: 'pixels',
           // @ts-ignore
@@ -28,14 +49,24 @@ class AnimatedCircleLayer extends CompositeLayer<any, any> {
         this.getSubLayerProps({
           id: 'circle-overlay',
           data,
-          pointType: 'circle',
+          pointType: 'circle+text',
           stroked: false,
           pointRadiusScale: 1,
+          pickable: false,
           pointRadiusUnits: 'pixels',
           // @ts-ignore
           getPointRadius: (d) =>
             scalePoints(d.properties.aggregated_count) * 1.75,
           getFillColor: [51, 218, 255, 25],
+          // text-options
+          getText: (d) => generateText(d.properties, keys),
+          getTextSize: 16,
+          getTextOffset: [5, 2],
+          getTextColor: [255, 255, 255, 255],
+          getTextBackgroundColor: [0, 0, 0, 150],
+          getTextBackgroundPadding: [10, 10],
+          textBackground: true,
+          textBillboard: true,
         }),
       ),
       new GeoJsonLayer(
@@ -47,6 +78,7 @@ class AnimatedCircleLayer extends CompositeLayer<any, any> {
           stroked: true,
           filled: false,
           pointRadiusScale: 1,
+          pickable: false,
           pointRadiusUnits: 'pixels',
           // @ts-ignore
           getPointRadius: (d) =>
